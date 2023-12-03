dfb22869d3bca29a612df6473cc22ecf90cd362f
Shrish Shete <107463412+shrish-shete20@users.noreply.github.com>
2023-12-03 21:43:43 +0530

    Update Recent changes.R

--- weblate/Recent Changes/Recent changes.R
+++ weblate/Recent Changes/Recent changes.R
@@ -6,7 +6,7 @@ library(readr)
 library(curl)
 library(lubridate)
 Language_Statistics <- read_csv("./../Language Statisitics/Language_Statistics_new.csv")
-api_token <- "wlu_s7fqhH2f9VgCCvIU2FQFlFMIZ27IH9GJwCg0"
+api_token <- Sys.getenv("WEBLATE_TOKEN")
 
 changes_url<-"https://translate.rx.studio/api/changes/?action=5"
 endpoint <- changes_url
