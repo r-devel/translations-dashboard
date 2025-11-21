303fe6520f5b4fd038bbd5b6f72f79646e15d9bf
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2025-11-21 13:12:25 +1100

    add fncs to fetch all pages from endpoint
    - calculate number of pages from initial endpoint response -- calcluate_n_pages()
    - fetch response content for each page and append to list -- fetch_pages_content()

--- weblate/R/fetch_responses.R
+++ weblate/R/fetch_responses.R
@@ -1,8 +1,28 @@
-fetch_response_as_json <- function(endpoint, handle) {
+fetch_response_content <- function(endpoint, handle) {
   print(paste("Querying endpoint", endpoint))
 
   response <- curl_fetch_memory(endpoint, handle = h)
   response_content <- rawToChar(response$content)
-
   fromJSON(response_content)
+}
+
+calculate_n_pages <- function(count, page_size = 50) {
+  remain <- count %% page_size
+
+  if (remain == 0) {
+    pages <- count / page_size
+  } else {
+    pages <- ceiling(count / page_size)
+  }
+
+  return(pages)
+}
+
+fetch_pages_content <- function(n_pages, endpoint, handle) {
+  pages <- vector("list", n_pages)
+  for (i in 1:n_pages) {
+    url <- paste0(endpoint, "&page=", i)
+    pages[[i]] <- fetch_response_content(url, handle)
+  }
+  pages
 }
