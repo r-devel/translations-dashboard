c63ee65da893c644620f07459f11251dfede9547
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2025-11-21 15:33:15 +1000

    styling mark_page

--- weblate/R/mark_page.R
+++ weblate/R/mark_page.R
@@ -11,37 +11,43 @@
 #'
 #' @examples
 #' \dontrun{
-#' mark_page(1, 
-#'   "https://translate.rx.studio/api/units/?q=project:r-project%20AND%20state:needs-editing", 
+#' mark_page(1,
+#'   "https://translate.rx.studio/api/units/?q=project:r-project%20AND%20state:needs-editing",
 #'   Language_Statistics)
 #' }
-mark_page <- function (page_no, edit_url, language_file = NULL) {
-  
+mark_page <- function(page_no, edit_url, language_file = NULL) {
   # input checking
-  stopifnot("page should be greater than 0" = page_no > 0,
-            "edit_url should not be empty" = nzchar(edit_url), 
-            "language_file must be provided" = !is.null(language_file))
-  
+  stopifnot(
+    "page should be greater than 0" = page_no > 0,
+    "edit_url should not be empty" = nzchar(edit_url),
+    "language_file must be provided" = !is.null(language_file)
+  )
+
   mark_url <- paste0(edit_url, "&page=", page_no)
-  mark_changes <- fetch_response_as_json(endpoint = mark_url, handle = h)
+  mark_changes <- fetch_response_content(endpoint = mark_url, handle = h)
   # each row is a unit: https://docs.weblate.org/en/latest/api.html#units
-  
-  mark_lang <- match_language_names(mark_changes$results$language_code, 
-                                    language_file)
-  
-  mark_lib_id <- match(basename(dirname(mark_changes$results$translation)),
-                       slugs)
+
+  mark_lang <- match_language_names(
+    mark_changes$results$language_code,
+    language_file
+  )
+
+  mark_lib_id <- match(
+    basename(dirname(mark_changes$results$translation)),
+    slugs
+  )
   mark_lib <- name_of_libraries[mark_lib_id]
-  
+
   # where there are multiple messages due to plurals, use the first
   mark_string <- vapply(mark_changes$results$source, "[", character(1), 1)
   mark_units <- mark_changes$results$id
   mark_web_url <- mark_changes$results$web_url
-  
-  data.frame(language = mark_lang,
-             library = mark_lib,
-             string = mark_string,
-             id = mark_units,
-             url = mark_web_url)
-  
+
+  data.frame(
+    language = mark_lang,
+    library = mark_lib,
+    string = mark_string,
+    id = mark_units,
+    url = mark_web_url
+  )
 }
